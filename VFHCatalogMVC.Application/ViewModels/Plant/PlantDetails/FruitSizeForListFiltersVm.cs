7b0646c5db1d39d114793ca73b6cbe50fb329cd7
Magdalena Pietryszak <magda13288@gmail.com>
2025-01-01 22:45:23 +0100

    Added VM's

---
+++ VFHCatalogMVC.Application/ViewModels/Plant/PlantDetails/FruitSizeForListFiltersVm.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VFHCatalogMVC.Application.Mapping;
+using VFHCatalogMVC.Application.ViewModels.Plant.Common;
+using VFHCatalogMVC.Domain.Model;
+
+namespace VFHCatalogMVC.Application.ViewModels.Plant.PlantDetails
+{
+    public class FruitSizeForListFiltersVm:PlantPropertyForListFiltersVm, IMapFrom<Domain.Model.FruitSizeForListFilters>
+    {
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Model.FruitSizeForListFilters, FruitSizeForListFiltersVm>().ReverseMap()
+                .ForMember(m=>m.FruitSizeId, opt=>opt.MapFrom(d=>d.Id));
+
+        }
+    }
+}
---
+++ VFHCatalogMVC.Application/ViewModels/Plant/PlantDetails/FruitTypeForListFiltersVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VFHCatalogMVC.Application.Mapping;
+using VFHCatalogMVC.Application.ViewModels.Plant.Common;
+
+namespace VFHCatalogMVC.Application.ViewModels.Plant.PlantDetails
+{
+    public class FruitTypeForListFiltersVm:PlantPropertyForListFiltersVm, IMapFrom<Domain.Model.FruitTypeForListFilters>
+    {
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Model.FruitTypeForListFilters, FruitTypeForListFiltersVm>().ReverseMap()
+                .ForMember(m => m.FruitTypeId, opt => opt.MapFrom(d => d.Id));
+
+        }
+    }
+}
